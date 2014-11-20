2ce29021ce53ab4605a6f25425bdd94cb99bdb25
Neamar <neamar@neamar.fr>
2014-11-20 17:58:09 +0100

    Underscore FTW

--- bin/loggers/restify.js
+++ bin/loggers/restify.js
@@ -1,41 +1,19 @@
 "use strict";
-var util = require('util');
+var _ = require('underscore');
+
+
+// Who want ERB? (not me)
+// Let's go for mustache style parsing!
+_.templateSettings = {
+  interpolate: /\{\{(.+?)\}\}/g
+};
+
+
+var template = "{{ req.method }} {{ req.user || '???' }} {{ req.url }} {{ res.status }} {{ res.time }}ms {{ res.restCode }} {{ msg }}";
+
+var compiledTemplate = _.template(template);
 
 
 module.exports = function(rec, stylize, emit) {
-  if(!rec.req || !rec.res) {
-    return emit(JSON.stringify(rec) + "\n");
-  }
-
-  // Base
-  var line = stylize(util.format('%s %s:%s', rec.req.method, (rec.req.user || "???"), rec.req.url), 'grey');
-
-  // Status code
-  var statusColor = 'green';
-  if(rec.res.status >= 500) {
-    statusColor = 'red';
-  }
-  else if(rec.res.status >= 400) {
-    statusColor = 'yellow';
-  }
-  else if(rec.res.status >= 300) {
-    statusColor = 'cyan';
-  }
-
-  line += " " + stylize(rec.res.status, statusColor);
-
-  if(rec.res.time < 30) {
-    line += " " + stylize(rec.res.time + "ms", "grey");
-  }
-  else {
-    line += " " + stylize(rec.res.time + "ms", "bold");
-  }
-
-  if(rec.res.restCode) {
-    line += " " + stylize(rec.res.restCode, 'underline');
-  }
-  if(rec.msg) {
-    line += " " + stylize(rec.msg, '');
-  }
-  emit(line + "\n");
+  emit(compiledTemplate(rec) + "\n");
 };
