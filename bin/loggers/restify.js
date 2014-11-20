6bd4a484d9db17e11617bd16ab790218e7ea5789
Neamar <neamar@neamar.fr>
2014-11-20 18:23:35 +0100

    Using colors

--- bin/loggers/restify.js
+++ bin/loggers/restify.js
@@ -9,11 +9,16 @@ _.templateSettings = {
 };
 
 
-var template = "{{ req.method }} {{ req.user || '???' }} {{ req.url }} {{ res.status }} {{ res.time }}ms {{ res.restCode }} {{ msg }}";
+var template = "{{ cS('grey') }}{{ req.method }} {{ req.user || '???' }} {{ req.url }}{{ cE('grey') }} {{ cS(res.status > 500 ? 'red' : res.status > 400 ? 'yellow' : res.status > 300 ? 'cyan' : 'green')}}{{ res.status }}{{ cE('green') }} {{ cS(res.time > 800 ? 'bold': 'grey')}}{{ res.time }}ms{{ cE(res.time > 800 ? 'bold': 'grey') }} {{ cS('underline') }}{{ res.restCode }}{{ cE('underline')}} {{ msg }}";
 
 var compiledTemplate = _.template(template);
 
 
-module.exports = function(rec, stylize, emit) {
+module.exports = function(rec, startStylizeWithColor, endStylizeWithColor, emit) {
+  // color start function
+  rec.cS = startStylizeWithColor;
+  // color end function
+  rec.cE = endStylizeWithColor;
+
   emit(compiledTemplate(rec) + "\n");
 };
